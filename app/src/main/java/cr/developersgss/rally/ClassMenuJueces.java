44cb6872fa0d35403ae95b9718bc0a48dc892e98
CarlosMora <carlosmt17@gmail.com>
2017-11-22 22:22:53 -0600

    modulos login jueces y uniones

---
+++ app/src/main/java/cr/developersgss/rally/ClassMenuJueces.java
@@ -0,0 +1,13 @@
+package cr.developersgss.rally;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassMenuJueces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_class_menu_jueces);
+    }
+}
--- app/src/main/java/cr/developersgss/rally/ClassMenuPrincipal.java
+++ app/src/main/java/cr/developersgss/rally/ClassMenuPrincipal.java
@@ -1,7 +1,9 @@
 package cr.developersgss.rally;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by germay on 21/11/2017.
@@ -13,5 +15,11 @@ public class ClassMenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interface_menuprincipal);
+    }
+    public void clickJueces(View view)
+    {
+        Intent intent =new Intent(ClassMenuPrincipal.this,ClassMenuJueces.class);
+        startActivity(intent);
+
     }
 }
