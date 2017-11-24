fee6558ea958d77fe71786623b4a3aafe8333e42
afi360 <afi_dragon@hotmail.com>
2017-11-23 23:19:59 -0600

    interfaces multiples modulos

---
+++ app/src/main/java/cr/developersgss/rally/ModuloAdministrador/ClassRallyAdministrador.java
@@ -0,0 +1,19 @@
+package cr.developersgss.rally.ModuloAdministrador;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cr.developersgss.rally.R;
+
+/**
+ * Created by Afi on 22/11/2017.
+ */
+
+public class ClassRallyAdministrador extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_rallyadministrador);
+    }
+}
